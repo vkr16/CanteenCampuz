64072b388ed2ca1633a3267eaf1671e4707f7d3d
Fikri Miftah A <fikri.droid16@gmail.com>
2023-04-02 14:00:01 +0700

    production ready

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeigniter4/appstarter',
-        'pretty_version' => 'v4.3.2',
-        'version' => '4.3.2.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6135c8cbe369dc79bccb67734edbdb25c5d6bf58',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'codeigniter4/appstarter' => array(
-            'pretty_version' => 'v4.3.2',
-            'version' => '4.3.2.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6135c8cbe369dc79bccb67734edbdb25c5d6bf58',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
